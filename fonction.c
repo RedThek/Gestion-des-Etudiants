a7e46c2d5268ef405f49f6189c72efce012c38d7
MOLA <91349290+RedThek@users.noreply.github.com>
2025-12-23 06:34:09 +0100

    Add core functionality for student management system including menu, student registration, modification, and listing

--- fonction.c
+++ fonction.c
@@ -1,2 +1,80 @@
-#include <stdio.h>
-#include "layout.h"
+#include "fonction.h"
+
+// Affiche le menu et retourne le choix
+int menu() {
+    int choix;
+    printf("\n--- MENU GESTION DES ETUDIANTS ---\n");
+    printf("1. Enregistrer un etudiant\n");
+    printf("2. Modifier un etudiant\n");
+    printf("3. Rechercher (Lineaire - Matricule)\n");
+    printf("4. Supprimer un etudiant\n");
+    printf("5. Trier par ordre Alphabetique (Nom)\n");
+    printf("6. Rechercher par Dichotomie (Matricule)\n");
+    printf("7. Calculer l'Age d'un etudiant\n");
+    printf("8. Trier par Filiere\n");
+    printf("9. Afficher la liste des etudiants\n");
+    printf("0. Quitter\n");
+    printf("Votre choix: ");
+    scanf("%d", &choix);
+    return choix;
+}
+
+// 1. Enregistrer
+void saisirEtudiant(Etudiant *e) {
+    printf("\nMatricule: "); scanf("%s", e->matricule);
+    printf("Nom: "); scanf("%s", e->nom);
+    printf("Prenom: "); scanf("%s", e->prenom);
+    printf("Date Naissance (JJ MM AAAA): "); 
+    scanf("%d %d %d", &e->dateNaissance.jour, &e->dateNaissance.mois, &e->dateNaissance.annee);
+    printf("Sexe (M/F): "); scanf("%s", e->sexe);
+    printf("Departement: "); scanf("%s", e->departement);
+    printf("Filiere: "); scanf("%s", e->filiere);
+    printf("Region d'origine: "); scanf("%s", e->regionOrigine);
+}
+
+// 9. Afficher la liste
+void afficherListe(Etudiant *tab, int n) {
+    printf("\n--- LISTE DES ETUDIANTS ---\n");
+    for(int i = 0; i < n; i++) {
+        printf("[%d] %s %s (Matricule: %s, Filiere: %s)\n", 
+               i+1, tab[i].nom, tab[i].prenom, tab[i].matricule, tab[i].filiere);
+    }
+}
+
+// 2. Modifier (avec pointeurs)
+void modifierEtudiant(Etudiant *e) {
+
+}
+
+// 7. Calculer Age
+int calculerAge(Date dateNaiss) {
+}
+
+// 3. Recherche Linéaire
+int rechercherMatricule(Etudiant *tab, int n, char *matricule) {
+}
+
+// 5. Tri Alphabétique
+void trierAlphabetique(Etudiant *tab, int n) {
+}
+
+// 4. Supprimer
+void supprimerEtudiant(Etudiant *tab, int *n, char *matricule) {
+}
+
+// 6. Recherche Dichotomique (Nécessite un tableau trié par Matricule au préalable)
+int rechercheDichotomique(Etudiant *tab, int n, char *matricule) {
+}
+
+// 8. Trier par Filière
+void trierFiliere(Etudiant *tab, int n) {
+}
+
+// Sauvegarde le tableau d'étudiants dans le fichier
+void sauvegarderEtudiants(Etudiant *tab, int n) {
+}
+
+// Charge les étudiants depuis le fichier dans le tableau
+// Retourne le nombre d'étudiants chargés
+int chargerEtudiants(Etudiant *tab) {
+}
--- fonction.h
+++ fonction.h
@@ -1,67 +1,43 @@
 #ifndef FONCTION_H
 #define FONCTION_H
 
-//Definition des structures date et etudiant
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-typedef struct Date
-{
+typedef struct {
     int jour;
     int mois;
     int annee;
-}Date;
+} Date;
 
-typedef struct Etudiant
-{
-    char matricule[11];
-    char nom[30];
-    char prenom[30];
+ 
+typedef struct {
+    char matricule[20];
+    char nom[50];
+    char prenom[50];
     Date dateNaissance;
-    char sexe[5];
-    char departement[20];
-    char filiere[20];
-    char regionOrigine[20];
-}Etudiant;
+    char sexe[10]; // "M" ou "F"
+    char departement[50];
+    char filiere[50];
+    char regionOrigine[50];
+} Etudiant;
 
-//Prototypes des fonctions
+// Prototypes des fonctions
 
-//Zone d'affichage
+int menu();
+void saisirEtudiant(Etudiant *e);
+void afficherEtudiant(Etudiant e);
+void afficherListe(Etudiant *tab, int n);
+void modifierEtudiant(Etudiant *e);
+int rechercherMatricule(Etudiant *tab, int n, char *matricule);
+void supprimerEtudiant(Etudiant *tab, int *n, char *matricule);
+void trierAlphabetique(Etudiant *tab, int n);
+void trierFiliere(Etudiant *tab, int n);
+int rechercheDichotomique(Etudiant *tab, int n, char *matricule);
+int calculerAge(Date dateNaiss);
+void sauvegarderEtudiants(Etudiant *tab, int n);
+int chargerEtudiants(Etudiant *tab);
 
-void afficherMenuPrincipal();
-void afficherEtudiant(Etudiant *etudiant);
-void afficherEtudiants(Etudiant *etudiants, int nombreEtudiants);
-
-//Zone d'enregistrement des etudiants
-
-void enregistrerEtudiant(Etudiant *etudiants);
-void enregistrerPlusieursEtudiants(Etudiant *etudiants, int nombreEtudiants);
-
-//Zone de recherche des etudiants
-int rechercherEtudiantParMatricule(Etudiant *etudiants, int nombreEtudiants, const char *matricule);
-void rechercherEtudiantsParNom(Etudiant *etudiants, int nombreEtudiants, const char *nom);
-void rechercherEtudiantsParFiliere(Etudiant *etudiants, int nombreEtudiants, const char *filiere);
-void rechercherEtudiantsParMotClef(Etudiant *etudiants, int nombreEtudiants, const char *motCle);
-
-//Zone de modification/suppression des etudiants
-
-void modifierEtudiant(Etudiant *etudiants, int nombreEtudiants, const char *matricule);
-void supprimerEtudiant(Etudiant *etudiants, int *nombreEtudiants, const char *matricule);
-
-//Zone de tri des etudiants
-void trierEtudiantsParFiliere(Etudiant *etudiants, int nombreEtudiants);
-void trierEtudiantsParNom(Etudiant *etudiants, int nombreEtudiants);
-void trierEtudiantsParDateNaissance(Etudiant *etudiants, int nombreEtudiants);
-
-//Zone de manipulation des fichiers
-
-void sauvegarderEtudiantsDansFichier(Etudiant *etudiants, int nombreEtudiants, const char *nomFichier);
-int chargerEtudiantsDepuisFichier(Etudiant *etudiants, const char *nomFichier);
-
-//Zone des statistiques
-
-int calculerAge(Etudiant etudiant);
-int calculerAgeMoyen(Etudiant *etudiants, int nombreEtudiants);
-int compterEtudiantsParFiliere(Etudiant *etudiants, int nombreEtudiants, const char *filiere);
-int compterEtudiantsParRegion(Etudiant *etudiants, int nombreEtudiants, const char *region);
-
-
-#endif // FONCTION_H
+#endif
